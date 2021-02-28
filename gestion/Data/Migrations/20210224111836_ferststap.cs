d5faa6da6b0c6ca250279edb9672329fe50b17ad
othmane-qr <othmane.qorqach@gmail.com>
2021-02-28 17:00:12 +0100

    add

---
+++ gestion/Data/Migrations/20210224111836_ferststap.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace gestion.Data.Migrations
+{
+    public partial class ferststap : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Etudiant",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(nullable: true),
+                    CIN = table.Column<string>(nullable: true),
+                    Adresse = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Etudiant", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Etudiant");
+        }
+    }
+}
